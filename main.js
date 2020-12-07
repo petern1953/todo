4a7077d9b9b21e392035ecc95459d584e4744513
petern1953 <petern1953@gmail.com>
2020-12-07 16:10:56 +0100

    html skeleton

---
+++ main.js
@@ -0,0 +1,18 @@
+// Egy Teendőlista alkalmazást kell lefejlesztened. Az alkalmazás designját és működését a todo1..todo3.jpg ábrák
+// (basic design, completed tasks, empty todo-list) szemléltetik.
+// A színeket, ikonokat és a betűtípust szabadon választhatod
+// A checkboxoknak nem kell egyedi designt készíteni.
+// Az alkalmazás localStorage-ban tárolja a teendőket.
+// Az alkalmazás indításkor ellenőrzi, vannak-e mentett teendők, ha igen, akkor betölti őket a storage-ból.
+// Felül szerepelnie kell a jelenlegi dátumnak.
+// Az input mező placholdere: "Take the garbage out"
+// A plusz jelre kattintva egy animáció kíséretében megjelenik az új teendő a listában, az input tartalma törlődik.
+// A teendőlista feletti mondatban (You have X pending items) látható, hogy szerepel a teendők száma, ez dinamikusan frissüljön mindig.
+// Ha egy teendő fölé visszük a kurzort, egy animáció kíséretében megjelenik a kuka ikon, amire kattintva törlődik a teendő.
+// A teendő előtti checkboxot bejelölve az adott teendő elvégzett lesz, a listáról eltűnik.
+// A Show / Hide Complete szövegre kattintva lehet megjeleníteni / elrejteni a már elvégzett teendőket.
+// A Clear All-ra kattintva a még el nem végzett összes teendő törlésre kerül.
+
+'use strict'
+
+console.log('hello')
